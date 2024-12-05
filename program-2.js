36e9c47fb912e283a21c7ab0dce7c880d3f72058
Tirth <tirth1943@gamil.com>
2024-12-05 14:57:50 +0530

    Test 6: Completed few programs

--- program-2.js
+++ program-2.js
@@ -3,22 +3,27 @@
 // Sample Input: [1, 2, [3, 4], [5, [6, 7], 8]];
 // Expected Output: [1, 2, 3, 4, 5, 6, 7];
 
+// function flattenArray(arr1) {
+//   let ans = [];
+//   addFlat(ans, arr1);
+//   return ans;
+// }
+
+// function addFlat(ans, arr) {
+//   curr = arr;
+//   for (let i = 0; i < curr.length; i++) {
+//     const element = arr[i];
+//     if (typeof element == "object") {
+//       addFlat(ans, element);
+//     } else {
+//       ans.push(element);
+//     }
+//   }
+// }
+
 function flattenArray(arr1) {
-  let ans = [];
-  addFlat(ans, arr1);
-  return ans;
+  return arr1.flat(Infinity);
 }
 
-function addFlat(ans, arr) {
-  curr = arr;
-  for (let i = 0; i < curr.length; i++) {
-    const element = arr[i];
-    if (typeof element == "object") {
-      addFlat(ans, element);
-    } else {
-      ans.push(element);
-    }
-  }
-}
 arr = [1, 2, [3, [4, 5]], [5, [6, 7]]];
 console.log(flattenArray(arr));
