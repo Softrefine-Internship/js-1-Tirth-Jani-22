58b9f8ecd682f3c08992d8e532ae8d32e3f3f43f
Tirth Jani <144887729+Tirth-Jani-22@users.noreply.github.com>
2024-12-06 15:37:38 +0530

    Update program-7.js

--- program-7.js
+++ program-7.js
@@ -1,19 +1,37 @@
-// Write javascript program to remove duplicate objects from an array.
+// // Write javascript program to remove duplicate objects from an array.
 
-// Sample Input:
-array = [
-  { title: "C++", author: "Bjarne" },
-  { title: "Java", author: "James" },
-  { title: "Python", author: "Guido" },
-  { title: "Java", author: "James" },
-];
+// // Sample Input:
+// array = [
+//   { title: "C++", author: "Bjarne" },
+//   { title: "Java", author: "James" },
+//   { title: "Python", author: "Guido" },
+//   { title: "Java", author: "James" },
+// ];
 
-// Expected Output:
-// [
-//  { title: 'C++', author: 'Bjarne' },
-//  { title: 'Java', author: 'James' },
-//  { title: 'Python', author: 'Guido' }
-// ]
+// // Expected Output:
+// // [
+// //  { title: 'C++', author: 'Bjarne' },
+// //  { title: 'Java', author: 'James' },
+// //  { title: 'Python', author: 'Guido' }
+// // ]
+
+// function removeDubli(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     curr = arr.splice(i, 1);
+//     bool = false;
+//     for (let j = 0; j < arr.length; j++) {
+//       const ele = arr[j];
+
+//       if (ele.title == curr[0].title && ele.author == curr[0].author) {
+//         bool = true;
+//       }
+//     }
+//     if (!bool) arr.splice(i, 0, ...curr);
+//   }
+//   return arr;
+// }
+// console.log(removeDubli(array));
+
 
 function removeDubli(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -22,7 +40,7 @@ function removeDubli(arr) {
     for (let j = 0; j < arr.length; j++) {
       const ele = arr[j];
 
-      if (ele.title == curr[0].title && ele.author == curr[0].author) {
+      if (JSON.stringify(ele) == JSON.stringify(curr[0]) ) {
         bool = true;
       }
     }
@@ -30,4 +48,5 @@ function removeDubli(arr) {
   }
   return arr;
 }
-console.log(removeDubli(array));
+arr = [{id:1,num:[1,2,3]},{id:1,num:[1,2,3]},{id:2,num:[1,2,3,4]}]
+console.log(removeDubli(arr));
