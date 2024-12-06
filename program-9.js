c6f56f41e2259c523e64b74d3a207f67edd3e768
Tirth Jani <144887729+Tirth-Jani-22@users.noreply.github.com>
2024-12-06 16:09:14 +0530

    Update program-9.js

--- program-9.js
+++ program-9.js
@@ -1,6 +1,35 @@
+// // Write a JavaScript program to find the most frequent item in an array.
+
+// arr = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+// // Expected Output : console.log(a, "5 times")
+// function fun(arr) {
+//   let map = new Map();
+//   for (let i = 0; i < arr.length; i++) {
+//     const ele = arr[i];
+//     if (map.has(ele)) {
+//       val = map.get(ele);
+//       map.set(ele, val + 1);
+//     } else {
+//       map.set(ele, 1);
+//     }
+//   }
+//   console.log(map);
+//   min = 0;
+//   k = null;
+//   map.forEach((val, key, map) => {
+//     if (val > min) {
+//       k = key;
+//       min = val;
+//     }
+//   });
+//   console.log(k, " : ", min, " Times");
+// }
+
+// fun(arr);
+
 // Write a JavaScript program to find the most frequent item in an array.
 
-arr = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+arr = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3,3];
 // Expected Output : console.log(a, "5 times")
 function fun(arr) {
   let map = new Map();
@@ -14,6 +43,7 @@ function fun(arr) {
     }
   }
   console.log(map);
+  ans = []
   min = 0;
   k = null;
   map.forEach((val, key, map) => {
@@ -22,7 +52,17 @@ function fun(arr) {
       min = val;
     }
   });
-  console.log(k, " : ", min, " Times");
+  map.forEach((val, key, map) => {
+    if (val == min) {
+      ans.push({
+          key:key,
+          val:val
+      })
+    }
+  });
+  ans.map((ele)=>{
+      console.log(ele.key, " : ", ele.val, " Times");
+  })
 }
 
 fun(arr);
